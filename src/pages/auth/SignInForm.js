6fb872fcb8515cbe70600102456927f0d50609b2
Alma Isaksson <isakvent@outlook.com>
2024-01-30 17:03:11 +0000

    add signin form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,36 +1,91 @@
 import React, { useState } from "react";
-//import axios from 'axios';
+import axios from 'axios';
 //import styles from "../../styles/SignInForm.module.css";
 import {
     Form,
     Button,
+    Alert,
   } from "react-bootstrap";
 
+import { Link, useHistory } from "react-router-dom";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+
 function SignInForm() {
-  
+  const setCurrentUser = useSetCurrentUser();
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const handleSubmit = async (user_input) => {
+    user_input.preventDefault();
+
+    try {
+        const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+        setCurrentUser(data.user);
+        history.push("/");
+    } catch (err) {
+        setErrors(err.response?.data);
+    }
+  };
+
+  const handleInput = (event) => {
+    setSignInData({
+        ...signInData,
+        [event.target.name]: event.target.value,
+    });
+  };
 
     return (
         <div>
             <h1>Sign in</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
 
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                type="text"
-                placeholder=""
-                name="Username"
-                />
+                <Form.Group controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    value={username}
+                    onChange={handleInput}
+                    />
+                </Form.Group>
+                {errors.username?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                        {message}
+                    </Alert>
+                ))}
 
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                type="password"
-                placeholder=""
-                name="password"
-                />
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={handleInput}
+                    />
+                </Form.Group>
+                {errors.password?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                        {message}
+                    </Alert>
+                ))}
 
-                <Button>
+                <Button type="submit">
                     Sign in
                 </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                        {message}
+                    </Alert>
+                ))}
 
             </Form>
         </div>
